51854bfbba0467c980ee1bfcb42aa894730c78b6
Shahbaz Khan <shahbaz@transpacks.co>
2020-03-31 09:47:29 +0530

    Adds dependencies and gitignore

---
+++ install_tf_dependencies.sh
@@ -0,0 +1,29 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+#compiler tools
+sudo apt-get install build-essential cmake unzip pkg-config
+sudo apt-get install gcc-6 g++-6
+
+#screen
+sudo apt-get install screen
+
+#X Windows and OpenGL libraries
+sudo apt-get install libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+
+#image and video I/O
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libxvidcore-dev libx264-dev
+
+#optimization
+sudo apt-get install libopenblas-dev libatlas-base-dev liblapack-dev gfortran
+
+#hdf5
+sudo apt-get install libhdf5-serial-dev
+
+#Py3 dev, TK, GTK GUI
+sudo apt-get install python3-dev python3-tk python-imaging-tk
+sudo apt-get install libgtk-3-dev
+
+
